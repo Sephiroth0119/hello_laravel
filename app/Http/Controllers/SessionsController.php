fd53759d68fa4ec222261badf03f364296771bdd
vagrant <vagrant@homestead>
2020-02-02 16:08:11 +0000

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -11,8 +11,8 @@ class SessionsController extends Controller
     //只让未登录的用户访问
     public function __construct()
     {
-        $this->middleware('guest',[
-            'only'=>['create']
+        $this->middleware('guest', [
+            'only' => ['create']
         ]);
     }
 
@@ -30,22 +30,26 @@ class SessionsController extends Controller
 
         if (Auth::attempt($credentials, $request->has('remember'))) {
             //登录成功后的通知
-            session()->flash('success', '欢迎回来');
-            $fallback = route('users.show',Auth::user());
-            return redirect()->intended($fallback);
+            if (Auth::user()->activated) {
+                session()->flash('success', '欢迎回来');
+                $fallback = route('users.show', Auth::user());
+                return redirect()->intended($fallback);
+            } else {
+                Auth::logout();
+                session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活');
+                return redirect('/');
+            }
         } else {
             //登陆失败后的相关操作
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
-
-        return;
     }
 
     public function destory()
     {
         Auth::logout();
-        session()->flash('sucesss', '恁已成功退出');
+        session()->flash('sucesss', '您已成功退出');
         return redirect('login');
     }
 }
