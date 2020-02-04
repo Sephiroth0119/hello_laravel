4e23b7729cf818560a00764dd130b71323000a95
vagrant <vagrant@homestead>
2020-02-04 07:23:12 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,15 +1,31 @@
 @extends('layouts.default')
 @section('content')
-<div class="jumbotron">
-  <h1>hello sjh</h1>
-  <p class="lead">
-    Now you see <a href="http://www.liesinapril.com">test web page</a>example
-  </p>
-  <p>
-    story begin.
-  </p>
-  <p>
-  <a class="btn btn-lg btn-success" href=" {{ route('signup') }}" role="button">Register</a>
-  </p>
-</div>
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info',['user'=>Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+  <div class="jumbotron">
+    <h1>hello sjh</h1>
+    <p class="lead">
+      Now you see <a href="http://www.liesinapril.com">test web page</a>example
+    </p>
+    <p>
+      story begin.
+    </p>
+    <p>
+    <a class="btn btn-lg btn-success" href=" {{ route('signup') }}" role="button">Register</a>
+    </p>
+  </div>
+  @endif
+
 @endsection
