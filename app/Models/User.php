984bacda649d1867d271fa060f8bd15a4f56d437
vagrant <vagrant@homestead>
2020-02-04 13:27:06 +0000

    社交统计信息

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,18 +66,18 @@ class User extends Authenticatable
 
     public function followers()
     {
-        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
     }
 
     public function followings()
     {
-        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
     }
 
     public function follow($user_ids)
     {
         if (!is_array($user_ids)) {
-            $user_ids = compact('users_ids');
+            $user_ids = compact('user_ids');
         }
         $this->followings()->sync($user_ids, false);
     }
